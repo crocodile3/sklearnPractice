e8bbed3e7111cdd255c70220c08c93f92e9a3462
crocodile3 <yrt333333@163.com>
2018-11-08 15:34:35 +0800

    Initial commit

---
+++ demo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'Crocodile3'
+__mtime__ = '2018/11/8'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
